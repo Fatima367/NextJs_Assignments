6bd9f18ac2bb3d65e340b1857cfc0d4fed788c36
Fatima367 <princess.fatima3275@gmail.com>
2024-09-10 12:10:06 +0500

    step00_helloworld

---
+++ 01Assignment/assignmentone/app/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <body className="bg-teal-50">
+        <Image
+          className="dark:invert"
+          src="https://nextjs.org/icons/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />{" "}
+        <h1 className="font-bold"> Assignment </h1>
+        <br />
+        <br />
+        <h1 className="text-center font-serif font-bold text-7xl bg-white">
+          <i> Hello World! </i>
+        </h1>
+      </body>
+    </>
+  );
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# NextJs_Assignments
-Assignments made using ReactJs framework which is NextJs. 
