d11d2c6b016a2cda4e7100b5f6f62e58ba43e774
Fatima367 <princess.fatima3275@gmail.com>
2024-09-11 13:26:13 +0500

    step00_helloworld

--- 01Assignment/app/page.tsx
+++ 01Assignment/app/page.tsx
@@ -3,24 +3,22 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <body className="bg-teal-50">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />{" "}
-        <h1 className="font-bold"> Assignment </h1>
-        <br />
-        <br />
-       
-        <h1 className="text-center font-serif font-bold text-7xl bg-white">
+      <Image
+        className="dark:invert"
+        src="https://nextjs.org/icons/next.svg"
+        alt="Next.js logo"
+        width={180}
+        height={38}
+        priority
+      />{" "}
+      <h1 className="font-bold"> Assignment </h1>
+      <br />
+      <br />
+      <div className="bg-teal-50">
+        <h1 className="text-center font-serif font-bold text-7xl ">
           <i> Hello World! </i>
         </h1>
-       
-      </body>
+      </div>
     </>
   );
 }
