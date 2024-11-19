4ecf7e7a4dc99665282cc7cfd25afa053ad3571f
Fatima367 <princess.fatima3275@gmail.com>
2024-11-19 14:06:47 +0500

    10-assignment

--- 10_assignment/app/navbar/page.tsx
+++ 10_assignment/app/navbar/page.tsx
@@ -5,11 +5,12 @@ export default function Navbar() {
     <div className="bg-red-900 p-2 shadow-md fixed top-0 w-screen -ml-5 rounded-full">
       <div className="max-w-5xl mx-auto flex justify-start items-start text-white">
         <Link href="/">
-          <h1 className="text-2xl font-bold">Readventory</h1>
+          <h1 className="text-2xl font-bold sm:text-lg">Readventory</h1>
         </Link>
       </div>
       <div className="max-w-5xl mx-auto flex justify-end items-end -mt-9">
-        <div className="bg-white px-6 py-2 rounded-full shadow-lg border border-red-300">
+        <div className="bg-white px-6 py-2 rounded-full shadow-lg border border-red-300
+        sm:px-2">
           <nav className="flex gap-8">
             <Link href="/" className="text-red-900 font-semibold hover:underline">
               Home
