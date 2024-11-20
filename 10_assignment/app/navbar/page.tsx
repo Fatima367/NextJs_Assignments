f1485c5d6f0862612627d81baf68e7d121abf192
Fatima367 <princess.fatima3275@gmail.com>
2024-11-20 17:14:27 +0500

    10th-assignment

--- 10_assignment/app/navbar/page.tsx
+++ 10_assignment/app/navbar/page.tsx
@@ -5,23 +5,23 @@ export default function Navbar() {
     <div className="bg-red-900 p-2 shadow-md fixed top-0 w-screen rounded-full">
       <div className="max-w-5xl mx-auto flex justify-between items-center text-white px-4">
         <Link href="/">
-          <h1 className="text-xl font-bold sm:text-2xl">Readventory</h1>
+          <h1 className="text-base font-bold sm:text-2xl">Readventory</h1>
         </Link>
 
         <div
-          className="bg-white px-3 py-2 sm:px-8 sm:py-2 rounded-full shadow-lg border border-red-300 
-        flex gap-4 mr-5 sm:mr-0 -ml-2 sm:-ml-3"
+          className="bg-white px-2 py-1 sm:px-6 sm:py-2 rounded-full shadow-lg border border-red-300 
+        flex gap-4 mr-5 sm:mr-0 ml-2 sm:-ml-3"
         >
-          <nav className="flex gap-4 sm:gap-7">
+          <nav className="flex gap-2 sm:gap-7">
             <Link
               href="/"
-              className="text-red-900 font-semibold hover:underline text-sm sm:text-md"
+              className="text-red-900 font-semibold hover:underline text-xs sm:text-base"
             >
               Home
             </Link>
             <Link
               href="/#addbook"
-              className="text-red-900 font-semibold hover:underline text-sm sm:text-md"
+              className="text-red-900 font-semibold hover:underline text-xs sm:text-base"
             >
               Add New Book
             </Link>
