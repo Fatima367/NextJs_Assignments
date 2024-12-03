9b284db2a7b07c40fc932f7628cd0c1d70f9d534
Fatima367 <princess.fatima3275@gmail.com>
2024-12-03 16:40:40 +0500

    12th-assignment

--- 12_assignment/app/page.tsx
+++ 12_assignment/app/page.tsx
@@ -1,6 +1,24 @@
+import { SessionWrapper } from "@/components/SessionWrapper";
+import SignOutPage from "@/components/SignOutComponent";
+import AuthComponent from "@/components/login/AuthComponent";
+import { auth } from "@/lib/auth";
 
 export default async function Login() {
+  const session = await auth();
+  const isAuthenticated = !!session?.user;
 
-  
-
+  return (
+    <div>
+      <SessionWrapper>
+        {isAuthenticated
+          ? <SignOutPage />
+          : <AuthComponent />
+        }
+      </SessionWrapper>
+    </div>
+  );
 }
+
+// export default function Home() {
+//   return <div>Clerk Authentication</div>;
+// }
--- 12_assignment/lib/auth.ts
+++ 12_assignment/lib/auth.ts
@@ -1,12 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import GithubProvider from "next-auth/providers/github"
 
-
-export const { handlers, auth, signIn, signOut } = NextAuth({
+export const { handlers, auth, signIn, signOut} = NextAuth({
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
         })
     ]
-});
+})
