fc1665dfa86d42e3b52aa74f29903345d89da55e
Fatima367 <princess.fatima3275@gmail.com>
2024-12-03 17:40:56 +0500

    13th-assignment

---
+++ 13_assignment/components/LoginForm.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React, { useState } from "react";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+
+const LoginForm = () => {
+
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+
+    const handleSubmit = (e : React.FormEvent) => {
+        e.preventDefault();
+        
+        console.log("Login attempt with :",{ email, password })
+    };
+
+    return(
+        <Card className="w-[350px]">
+            <CardHeader>
+                <CardTitle>LogIn</CardTitle>
+                <CardDescription>Enter your credentials to access your account</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <div className="space-y-2">
+                        <Input 
+                        value={email}
+                        type="email"
+                        placeholder="Email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        />
+                    </div>
+                    <div className="space-y-2">
+                        <Input 
+                        value={password}
+                        type="password"
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        />
+                    </div>
+                    <Button type="submit" className="w-full" onClick={() => alert("Thanks for Login!")}>
+                        Login
+                    </Button>
+                </form>
+            </CardContent>
+        </Card>
+    )
+};
+
+export default LoginForm;
---
+++ 13_assignment/components/SignupForm.tsx
@@ -0,0 +1,64 @@
+"use client";
+import React, { useState } from "react";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+
+const SignupForm = () => {
+
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ confirmPassword, setConfirmPassword ] = useState("");
+
+    const handleSubmit = (e : React.FormEvent) => {
+        e.preventDefault();
+        
+        console.log("Signup attempt with :",{ email, password, confirmPassword })
+    };
+
+    return(
+        <Card className="w-[350px]">
+            <CardHeader>
+                <CardTitle>Signup</CardTitle>
+                <CardDescription>Enter your details to create a new account</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <div className="space-y-2">
+                        <Input 
+                        value={email}
+                        type="email"
+                        placeholder="Email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        />
+                    </div>
+                    <div className="space-y-2">
+                        <Input 
+                        value={password}
+                        type="password"
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        />
+                    </div>
+                    <div className="space-y-2">
+                        <Input 
+                        value={confirmPassword}
+                        type="password"
+                        placeholder="Confirm Password"
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        required
+                        />
+                    </div>
+                    <Button type="submit" className="w-full" onClick={() => alert("Thanks for Signing Up!")}>
+                        Signup
+                    </Button>
+                </form>
+            </CardContent>
+        </Card>
+    )
+};
+
+export default SignupForm;
