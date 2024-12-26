30a143fff77bf1b64498452d5d16a2b472a8b56f
Fatima367 <princess.fatima3275@gmail.com>
2024-12-26 10:48:32 +0500

    13th-assignment

--- 13_assignment/app/layout.tsx
+++ 13_assignment/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ClerkProvider, SignedOut, SignInButton } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import Nav from "./navbar/page";
 
 const geistSans = localFont({
