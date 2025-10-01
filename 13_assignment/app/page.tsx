2ff3a5e0006c0e83cbfaf7bcadee3a010157520c
Fatima367 <170196704+Fatima367@users.noreply.github.com>
2025-10-01 12:19:17 +0500

    removed unused code

--- 13_assignment/app/page.tsx
+++ 13_assignment/app/page.tsx
@@ -8,30 +8,3 @@ export default function Home() {
     </div>
   );
 }
-
-
-
-//Class work:
-// 'use client';
-// import { Button } from "@/components/ui/button";
-// import Link from "next/link";
-
-// export default function Home() {
-
-//   return (
-//     <div className="bg-slate-950 font-serif w-screen min-h-screen">
-//       <div className="mx-auto items-center justify-center flex flex-col">
-//         <h1 className="text-cyan-300 text-4xl font-bold mt-5">
-//           Welcome to TechHive
-//         </h1>
-//       </div>
-//       <div className="mt-4 flex flex-shrink mx-auto">
-//         <Link href="./user">
-//         <Button variant="destructive" className="ml-16">
-//           Sign To Web
-//           </Button>
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// }
